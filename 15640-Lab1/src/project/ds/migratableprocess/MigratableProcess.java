c682f2c79e0fd49f0083ee357cf65ecca113e565
rohitju <rohitju07@gmail.com>
2014-08-30 01:59:52 -0400

    Commit process code

--- 15640-Lab1/src/project/ds/migratableprocess/MigratableProcess.java
+++ 15640-Lab1/src/project/ds/migratableprocess/MigratableProcess.java
@@ -3,5 +3,10 @@ package project.ds.migratableprocess;
 import java.io.Serializable;
 
 public interface MigratableProcess extends Runnable, Serializable {
+	
+	public void run();
+	
+	public void suspend();
 
+	
 }
